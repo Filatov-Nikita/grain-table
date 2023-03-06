01b397816bc0df0511abf0d638c4002877bd53d1
nikita <nikita45454@gmail.com>
2023-03-06 15:19:35 +0500

    feat: изменяет конфиг

--- src/env.js
+++ src/env.js
@@ -1,3 +1,4 @@
 export const API_Domain = "http://62.133.189.86:8080";
 export const TableData_Path = "/demo/balances";
-export const Prod_Path = "/extrusion-workshop/balances";
+// export const Prod_Path = "/extrusion-workshop/balances";
+export const Prod_Path = "http://62.133.189.86:8080/demo/balances";
